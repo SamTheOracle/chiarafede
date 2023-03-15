007fc9355f39b5d187aab5741aaf52663c8386e0
SamTheOracle <gzano93@gmail.com>
2023-03-15 15:42:03 +0100

    heroku ssl redirect

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var express = require('express')
-var sslRedirect = require('heroku-ssl-redirect');
 var path = require('path')
+var sslRedirect = require('heroku-ssl-redirect').default;
 var serveStatic = require('serve-static')
+
 var app = express()
+
 
 // Use a fallback for non-root routes (required for Vue router)
 //   NOTE: History fallback must be "used" before the static serving middleware!
 // enable ssl redirect
 app.use(sslRedirect());
+
 // Serve static assets from the build files (images, etc)
 app.use(serveStatic(path.join(__dirname, '/dist')))
 
@@ -15,7 +18,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Server listening to port ${port}`)
