93ee6584604dc857c0e97752c28f6c8e418067be
SamTheOracle <gzano93@gmail.com>
2023-03-15 21:49:58 +0100

    new heart and icons

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -34,6 +34,12 @@
         <p class="text-center cardtitle mb-1">Via castello 15, Poggio Torriana (RN)</p>
       </v-col>
       <v-col cols=12>
+        <v-img src="@/assets/img/heart.png" contain
+          :style="$vuetify.breakpoint.smAndUp ? 'max-height:150px;;' : 'max-height:100px'" />
+      </v-col>
+      <v-col cols=12>
+        <v-img src="@/assets/img/phone-book.png" contain class="ma-10"
+          :style="$vuetify.breakpoint.smAndUp ? 'max-height:150px;;' : 'max-height:100px'" />
         <p class="text-center cardtitle-contacts mb-1">è gradita gentile conferma entro il 12 maggio 2023 al +39
           3661167291 o +39 3388461341</p>
       </v-col>
@@ -44,7 +50,7 @@
       <v-col cols=12 class="mt-16">
         <p class="text-center cardtitle-quote mb-1">"Dicono che i soldi non possono comprare la felicità...ma ti permettono di regalarla!"</p>
         <p class="text-center cardtitle-ramimalek mb-1">Rami Malek - Freddy Mercury</p>
-        <v-img src="@/assets/img/gift.png" contain class="ma-10"
+        <v-img src="@/assets/img/gift-box.png" contain class="ma-10"
           :style="$vuetify.breakpoint.smAndUp ? 'max-height:150px;;' : 'max-height:100px'" />
         <p class="text-center cardtitle mb-1">Se anche tu vuoi contribuire a regalarci un po' di felicità:</p>
         <p class="text-center cardtitle mb-1">Iban: IT76T306988570100000001038</p>
